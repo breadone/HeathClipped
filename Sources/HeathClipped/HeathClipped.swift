b31915f22633340e193a2445acb506bc64ff694b
pradyun <pradyun.setti@gmail.com>
2022-09-21 11:55:33 +1200

    oh my god FINALLY i hate swift strings so much

--- Sources/HeathClipped/HeathClipped.swift
+++ Sources/HeathClipped/HeathClipped.swift
@@ -3,7 +3,8 @@ import Foundation
 /// Gets the url for the heathcliff comic for the specified date
 /// - Parameter date: Date to get the comic (defaults to today)
 public func getHeathCliff(for date: Date = Date()) throws -> String {
-    let imageURL: String = "lol"
+    var imageURL: String
+    let baseURL = "https://www.gocomics.com/heathcliff/"
     
     // split the date into ymd
     let stringDate = date.formatted(.iso8601).split(separator: "-")
@@ -12,7 +13,7 @@ public func getHeathCliff(for date: Date = Date()) throws -> String {
     let day: String = String(stringDate[2].dropLast(10))
     
     // fetch the comic data
-    guard let comicURL = URL(string: "https://www.gocomics.com/heathcliff/\(year)/\(month)/\(day)") else {
+    guard let comicURL = URL(string: baseURL+"\(year)/\(month)/\(day)") else {
         throw URLError(.badURL)
     }
     
@@ -22,7 +23,9 @@ public func getHeathCliff(for date: Date = Date()) throws -> String {
         throw URLError(.unknown)
     }
     
+    let urlStart = html.range(of: "data-image")!.upperBound
     
+    imageURL = html.suffix(from: urlStart).components(separatedBy: "\"")[1]
             
     return imageURL
 }
