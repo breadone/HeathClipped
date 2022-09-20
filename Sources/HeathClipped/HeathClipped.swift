764038c3c2b3cb95375f7cdb70ce0323299a5204
pradyun <pradyun.setti@gmail.com>
2022-09-21 10:44:17 +1200

    no idea why im doing this but making good progress

--- Sources/HeathClipped/HeathClipped.swift
+++ Sources/HeathClipped/HeathClipped.swift
@@ -1,6 +1,24 @@
-public struct HeathClipped {
-    public private(set) var text = "Hello, World!"
+import Foundation
 
-    public init() {
+/// Gets the url for the heathcliff comic for the specified date
+/// - Parameter date: Date to get the comic (defaults to today)
+func getHeathCliff(for date: Date = Date()) {
+    // split the date into ymd
+    let stringDate = date.formatted(.iso8601).split(separator: "-")
+    let year: String = String(stringDate[0])
+    let month: String = String(stringDate[1])
+    let day: String = String(stringDate[2].dropLast(10))
+    
+    // fetch the comic data
+    let comicURL = URL(string: "https://www.gocomics.com/heathcliff/\(year)/\(month)/\(day)")!
+    let html: String
+    
+    do {
+        html = try String(contentsOf: comicURL)
+    } catch {
+        fatalError("Failed to parse URL: \(error.localizedDescription)")
     }
+    
+    print(html)
+    
 }
--- Tests/HeathClippedTests/HeathClippedTests.swift
+++ Tests/HeathClippedTests/HeathClippedTests.swift
@@ -2,10 +2,7 @@ import XCTest
 @testable import HeathClipped
 
 final class HeathClippedTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(HeathClipped().text, "Hello, World!")
+    func testDateSplitting() throws {
+        getHeathCliff()
     }
 }
